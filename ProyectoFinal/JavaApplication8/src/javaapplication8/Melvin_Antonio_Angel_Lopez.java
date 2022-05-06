5523b1103d05af25019fb91a4470cb28d2dd8eda
melvinantonio <melvinantonioangellopez@gmail.com>
2022-05-06 14:20:28 -0600

    Clase terminada

---
+++ ProyectoFinal/JavaApplication8/src/javaapplication8/Melvin_Antonio_Angel_Lopez.java
@@ -0,0 +1,10 @@
+package javaapplication8;
+
+public class Melvin_Antonio_Angel_Lopez {
+    
+    public class guitarra extends Instrumento{
+    public void tocar(){
+        System.out.println("La posición ideal para tocar la guitarra es sentado en una silla no muy alta");
+    }
+}
+}
